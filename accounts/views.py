c082ac71de31d40a27c5c0ead3b238c8f888ae56
devbittu <devbittu@proton.me>
2023-10-31 17:31:07 +0530

    add accounts

---
+++ accounts/views.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+class Login(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            messages.info(request, "You are already login. Logout first")
+            return redirect("index")
+        return render(request, "account/login.html")
+
+    def post(self, request):
+        uname = request.POST.get("username", "")
+        passwd = request.POST.get("password", "")
+        user = authenticate(username=uname, password=passwd)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in")
+            return redirect("index")
+        else:
+            messages.warning(request, "Username or password is incorrect")
+        return render(request, "account/login.html")
+
+@method_decorator(login_required, name="dispatch")
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect("login")
+
+class Register(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            messages.info(request, "You are already logged in")
+            return redirect("index")
+        return render(request, "account/register.html")
+    
+    def post(self, request):
+        uname = request.POST.get("username", "")
+        passwd = request.POST.get("password", "")
+        user = authenticate(username=uname, password=passwd)
+        if user is None:
+            user = User(username=uname)
+            user.set_password(passwd)
+            user.save()
+            messages.success(request, "User created")
+            return redirect("login")
+        else:
+            messages.info(request, "User already exists.")
+            return redirect("register")
