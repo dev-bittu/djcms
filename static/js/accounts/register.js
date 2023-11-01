a18ccb0549774813f97a41f933b16ec2c5ae6294
devbittu <devbittu@proton.me>
2023-11-01 06:35:01 +0530

    add ui

---
+++ static/js/accounts/register.js
@@ -0,0 +1,18 @@
+let form = document.querySelector('form');
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  var password1 = document.getElementById("password1").value;
+  var password2 = document.getElementById("password2").value;
+
+  alert(password1===password2);;
+  if (password1 === password2) {
+    form.submit();
+  } else {
+    msgbox = document.getElementById("msg-box");
+    msgbox.innerHTML += `<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>WARNING</strong> Passwords not match<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+    document.getElementById("password2").focus();
+  }
+}
+
