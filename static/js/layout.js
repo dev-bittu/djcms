578cdf02932d8474ee1c5c5d76ac21fe66348616
devbittu <devbittu@proton.me>
2023-11-17 17:22:57 +0530

    dont ask from me

--- static/js/layout.js
+++ static/js/layout.js
@@ -3,7 +3,7 @@ var register = document.querySelector('#registerBtn');
 
 if (window.matchMedia("(min-width: 992px)").matches) {
     login.classList.remove("nav-link");
-    login.classList.add('btn', 'btn-outline-light', 'rounded-pill');
+    login.classList.add('btn', 'btn-outline-light', 'rounded-pill', 'me-2');
 
     register.classList.remove("nav-link");
     register.classList.add('btn', 'btn-light', 'rounded-pill');
