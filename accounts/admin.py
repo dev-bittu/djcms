ec82e2c7f9bd18058cf31a9553c35a42f0626992
devbittu <devbittu@proton.me>
2023-11-17 21:10:43 +0530

    add is_author to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,6 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["username", "email", "is_staff"]
+    list_display = ["username", "email", "is_staff", "is_author"]
 
 #admin.site.register(User, UserAdmin)
