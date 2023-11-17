de817e632a26a49053da3234cd476e26baa4b0bc
devbittu <devbittu@proton.me>
2023-11-17 21:09:05 +0530

    improve messages and remove unused info

--- accounts/views.py
+++ accounts/views.py
@@ -7,11 +7,11 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import User
 
-# Create your views here.
+
 class Login(View):
     def get(self, request):
         if request.user.is_authenticated:
-            messages.info(request, "You are already login. Logout first")
+            messages.info(request, "You are already logged in, Logout first")
             return redirect("index")
         return render(request, "accounts/login.html")
 
@@ -45,13 +45,13 @@ class Register(View):
         data = request.POST
         passwd1 = data.get("password1", "")
         passwd2 = data.get("password2","")
-        if passwd1 != passwd2:
-            messages.info(request, "Passwords don't match")
+        if passwd1 and (passwd1 != passwd2):
+            messages.warning(request, "Passwords do not match")
             return redirect("accounts:register")
         
-        email, username = data.get("email", ""), data.get("username", "")
+        email, username = data.get("email"), data.get("username")
         if not (email and username):
-            messages.info(request, "Email required")
+            messages.info(request, "Email and username required")
             return redirect("accounts:register")
         
         user = User.objects.filter(Q(email=email) | Q(username=username))
