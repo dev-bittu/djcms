1ba96f41d3f6ab2e54ac4f4ebaa448ce3bc06f6e
devbittu <devbittu@proton.me>
2023-11-18 08:40:30 +0530

    add popover message when copied or failed

--- static/js/blogs/blog.js
+++ static/js/blogs/blog.js
@@ -1,9 +1,20 @@
 const copyUrl = () => {
   const url = window.location.href;
+  const copyBtn = document.getElementById("copyBtn");
   navigator.clipboard.writeText(url)
   .then(() => {
-    alert('URL copied to clipboard');
+    const popover = new bootstrap.Popover(copyBtn, {
+      content: 'URL copied to clipboard',
+      trigger: 'manual'
+    });
+    popover.show();
+    setTimeout(() => popover.hide(), 2000); // Hide the popover after 2 seconds
   }).catch(err => {
-    alert('Failed to copy: ', err);
+    const popover = new bootstrap.Popover(copyBtn, {
+      content: 'Failed to copy: ' + err,
+      trigger: 'manual'
+    });
+    popover.show();
+    setTimeout(() => popover.hide(), 2000); // Hide the popover after 2 seconds
   });
 }
