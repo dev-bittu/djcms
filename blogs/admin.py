ce019a2f4dd560ab7b17fc38ae24f93832ce407b
Dev-Bittu <devbittu@proton.me>
2023-11-26 19:51:32 +0530

    idm

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,19 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ["title", "likes", "published_on", "creator", "is_active"]
-    fields = ["title", "desc", "content", "categories", "thumbnail"]
+admin.site.register(Blog)
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["category"]
+admin.site.register(Category)
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ["comment", "blog", "likes", "published_on", "is_active"]
-    fields = ["comment", "blog"]
+admin.site.register(Comment)
 
 admin.site.register(Reply)
 
